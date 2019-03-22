45b94da9623b13467019921e13f677d8c8b2d4b2
marielcheto89 <marielcheto_89@abv.bg>
2019-03-22 07:30:13 +0200

    Mariela Text pages & About in progress.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,13 @@
 jQuery(document).ready(function($) {
-	
+	$('.js-team-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots: false,
+    items: 1,
+    navText: [
+    	'<svg width="15" height="27" viewBox="0 0 15 27" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 1L1 13.5L13.5 26" stroke="#132968" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+    	'<svg width="15" height="27" viewBox="0 0 15 27" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 1L14 13.5L1.5 26" stroke="#132968" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>'
+    ],
+	})
 });
--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,2 +1,2 @@
-jQuery(document).ready(function(e){});
+jQuery(document).ready(function(o){o(".js-team-carousel").owlCarousel({loop:!0,margin:10,nav:!0,dots:!1,items:1,navText:['<svg width="15" height="27" viewBox="0 0 15 27" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 1L1 13.5L13.5 26" stroke="#132968" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>','<svg width="15" height="27" viewBox="0 0 15 27" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 1L14 13.5L1.5 26" stroke="#132968" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>']})});
 //# sourceMappingURL=main.js.map
